fa2b232271d8cf4bf772962e30a1b546f576c831
Masdod <dodikunaedi46@gmail.com>
2025-05-08 19:12:03 +0700

    first commit

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <header className="bg-gradient-to-r from-amber-500 via-amber-600 to-amber-700 shadow-lg">
       <div className="flex md:flex-row flex-col justify-between md:items-center p-6 gap-4 max-w-7xl mx-auto">
         <Link href="/" className="font-bold text-white text-3xl hover:text-amber-300 transition-all">
-          ANIMEKU
+          CUYANIMELIST
         </Link>
         <div className="flex gap-4 items-center mt-2 md:mt-0">
           <InputSearch />
